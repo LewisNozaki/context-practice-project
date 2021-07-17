ca7a32b8a55a8834489b4a06818a67d7aae678e0
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-16 15:38:28 -1000

    updates

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import styles from './Navigation.module.css';
+import AuthContext from '../../store/auth-context'; // <-- our context state store.
 
 const Navigation = ({ isLoggedIn, onLogout }) => {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        {isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        
-        {isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        
-        {isLoggedIn && (
-          <li>
-            <button onClick={onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(contextData) => {
+        return (
+          <nav className={styles.nav}>
+            <ul>
+              {contextData.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              
+              {contextData.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              
+              {contextData.isLoggedIn && (
+                <li>
+                  <button onClick={onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        )
+      }}
+    </AuthContext.Consumer>
   );
 };
 
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,7 +7,13 @@ const AuthContext = React.createContext({
 
 export default AuthContext;
 
+// Providing: 
 // After creating the context with the initial state, we export it and then we wrap the context
 // component around any component that may need access to the state store. For this project, 
 // all components need access because we are using a simple authentication. So, we will go to App.js
-// and wrap it with the context component. 
+// and wrap it with the context component. <AuthContext.Provider>
+
+// Listening: 
+// After we provide the state store to our components, we need a way for the components to listen the state
+// and make changes to it accordingly. We can do that in two ways. We can use hooks or we can use the built in 
+// consumer method. <AuthContext.Consumer>. Look at the Navigation.js file for an example using the consumer method.
