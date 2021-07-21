25a733d3894dc8424e22d809832757f926f1e69d
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-21 11:07:50 -1000

    updates

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Navigation.module.css';
 import AuthContext from '../../store/auth-context'; // <-- our context state store.
 
 const Navigation = ({ onLogout }) => {
+  // the useContext hook 
+  // Save the return value into a new variable. 
+  // the useContext takes in the store we imported and returns an object of the store's value.
+  const contextData = useContext(AuthContext);
+  
   return (
-    <AuthContext.Consumer>
-      {(contextData) => {
-        return (
-          <nav className={styles.nav}>
-            <ul>
-              {contextData.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
+    // Using the .Consumer method
+    // <AuthContext.Consumer>
+    //   {(contextData) => {
+    //     return (
+    //       <nav className={styles.nav}>
+    //         <ul>
+    //           {contextData.isLoggedIn && (
+    //             <li>
+    //               <a href="/">Users</a>
+    //             </li>
+    //           )}
               
-              {contextData.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
+    //           {contextData.isLoggedIn && (
+    //             <li>
+    //               <a href="/">Admin</a>
+    //             </li>
+    //           )}
               
-              {contextData.isLoggedIn && (
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    //           {contextData.isLoggedIn && (
+    //             <li>
+    //               <button onClick={onLogout}>Logout</button>
+    //             </li>
+    //           )}
+    //         </ul>
+    //       </nav>
+    //     )
+    //   }}
+    // </AuthContext.Consumer>
+
+    // Using the hooks method
+    <nav className={styles.nav}>
+      <ul>
+        {contextData.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        
+        {contextData.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        
+        {contextData.isLoggedIn && (
+          <li>
+            <button onClick={onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
